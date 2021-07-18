578d94ed78d1354136cae52b9bd263356611d831
Natanael Soares <natanaelsoaresleite@gmail.com>
2021-07-17 23:22:16 -0300

    eliminando informações desnecessárias

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -1,11 +1,11 @@
 library(datadrivencv)
 library(here)
 use_datadriven_cv(
-  full_name = "Nick Strayer",
-  data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
-  pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
-  html_location = "nickstrayer.me/cv/",
-  source_location = "https://github.com/nstrayer/cv",
+  full_name = "Natanael Soares Leite",
+  data_location = "https://docs.google.com/spreadsheets/d/1LtfUnLPXcRZuNjlGvGKXYg3YjWuSh41NnWO-9pZmy_I",
+  pdf_location = "https://github.com/Natanaelsl/cv/raw/master/natanael_cv.pdf",
+  html_location = "https://natanaelsl.netlify.app/media/",
+  source_location = "https://github.com/Natanaelsl/cv",
   output_dir = here(),
   open_files = FALSE
 )
