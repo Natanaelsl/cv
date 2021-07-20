647b9ad2fac00b75b00ecdffeecc72706670c11e
Natanael Soares <natanaelsoaresleite@gmail.com>
2021-07-20 00:58:04 -0300

    corrigindo setup

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -3,8 +3,8 @@ library(here)
 use_datadriven_cv(
   full_name = "Natanael Soares Leite",
   data_location = "https://docs.google.com/spreadsheets/d/1LtfUnLPXcRZuNjlGvGKXYg3YjWuSh41NnWO-9pZmy_I",
-  pdf_location = "https://github.com/Natanaelsl/cv/raw/master/natanael_cv.pdf",
-  html_location = "https://natanaelsl.netlify.app/media/natanael_cv.pdf",
+  pdf_location = "https://natanaelsl.netlify.app/media/natanael_cv.pdf",
+  html_location = "https://natanaelsl.netlify.app/media/",
   source_location = "https://github.com/Natanaelsl/cv",
   output_dir = here(),
   open_files = FALSE
